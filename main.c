5c434d20da0640e3305b6b23c8d46cb67f87ac6e
ajman789 <aj.marchese@outlook.com>
2023-11-18 08:22:19 -0800

    added button to GUI

--- main.c
+++ main.c
@@ -1,27 +1,48 @@
 #include <gtk/gtk.h>
 
 static void
-activate(GtkApplication *app,
-         gpointer user_data)
+print_hello(GtkWidget *widget,
+            gpointer data)
 {
-    GtkWidget *window;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 400);
-    gtk_widget_show_all(window);
+    g_print("Hello World\n");
 }
 
 int main(int argc,
-         char **argv)
+         char *argv[])
 {
-    GtkApplication *app;
-    int status;
+    GtkBuilder *builder;
+    GObject *window;
+    GObject *button;
+    GError *error = NULL;
 
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+    gtk_init(&argc, &argv);
 
-    return status;
+    /* Construct a GtkBuilder instance and load our UI description */
+    builder = gtk_builder_new();
+    if (gtk_builder_add_from_file(builder, "builder.ui", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    /* Connect signal handlers to the constructed widgets. */
+    window = gtk_builder_get_object(builder, "window");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    button = gtk_builder_get_object(builder, "button1");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+    button = gtk_builder_get_object(builder, "button2");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+    button = gtk_builder_get_object(builder, "quit");
+    g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+    button = gtk_builder_get_object(builder, "clear");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+    gtk_main();
+
+    return 0;
 }
